00baa17129584d49d4766f9a59533557cec5e158
Ewert Daniel (CR/APA3) <ewd2rng@saz.bosch-si.com>
2021-11-25 16:37:25 +0100

    add legal note
    Signed-off-by: Daniel Ewert <daniel.ewert@de.bosch.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup, find_packages
+# Copyright (c) 2016 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository https://github.com/boschresearch/assets2036py.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from codecs import open
-from os import path, getenv
+from os import path
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
-
-BUILD_ID = "." + getenv("CI_BUILD_ID") if getenv("CI_BUILD_ID") else ""
 
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
@@ -12,7 +25,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='assets2036py',
-    version='0.0.12' + BUILD_ID,
+    version='0.0.12',
     url='https://github.com/boschresearch/assets2036-submodels',
     license='BIOS',
     author='Daniel Ewert (CR/APA3 G6/BD-BBI)',
