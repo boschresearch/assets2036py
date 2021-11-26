09f3162fdf2dcc0bb7aa802593fb1df997d23c88
Daniel Ewert <daniel.ewert@de.bosch.com>
2021-11-26 12:16:28 +0100

    Update setup.py
    trying to fix failing github action

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
