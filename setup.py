1b045dfcf68843e2fdcadbd84d96a77aeaae58d7
Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>
2024-02-22 10:16:39 +0100

    Update MQTTClient on_connect assignment
    Signed-off-by: Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="assets2036py",
-    version="0.1.6",
+    version="0.1.7",
     url="https://github.com/boschresearch/assets2036spy",
     license="BIOS",
     author="Daniel Ewert (CR/APT5)",
