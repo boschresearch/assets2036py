e8b06c19c1a12b436c764cf833b994a8e69b5c91
Daniel Ewert <daniel.ewert@de.bosch.com>
2024-02-22 17:05:43 +0100

    Logging handler improved (#14)
    * Refactor logging handler to use formatted log entry
    
    Signed-off-by: Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>
    
    * Update version to 0.1.9 in setup.py
    
    Signed-off-by: Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>
    
    ---------
    
    Signed-off-by: Ewert Daniel (CR/APT5) <daniel.ewert@bosch.com>

--- assets2036py/assetlogging.py
+++ assets2036py/assetlogging.py
@@ -1,15 +1,17 @@
 import logging
-from .assets import SubModel
 from logging import LogRecord
+
+from .assets import SubModel
 
 
 class AssetLoggingHandler(logging.Handler):
     """
     Custom Logging handler class to log via MQTT using the _endpoint of an asset.
     """
+
     def __init__(self, endpoint: SubModel, level: int = 0) -> None:
         super().__init__(level)
         self._endpoint = endpoint
 
     def emit(self, record: LogRecord) -> None:
-        self._endpoint.log(entry=record.msg)
+        self._endpoint.log(entry=self.format(record))
